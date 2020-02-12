8b8bdb59796351fefef1c50fc023e0fc2eebee25
Ian Jordan <59670817+jordan891@users.noreply.github.com>
2020-02-11 20:41:27 -0500

    Books feature (#17)
    * V2
    
    * navigation structure
    
    * v3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react" ;
-import "./App.cs " ;
+import "./App.css";
+import {Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import Routes from "./Routes";
 
 export default class App extends Component {
   render() {
-    return <div>Fisher Bookstore</div>;
+    return (
+      <div className = "App container">
+        <Navbar bg = "dark" expand = "jg">
+          <Navbar.Brand>
+            <Link to = "/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+        </Navbar>
+        <Routes />
+      </div>
+    )
   }
 }
