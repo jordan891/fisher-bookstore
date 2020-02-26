8925f11a58d4778243bd3962bd3c288e25b84a5d
Ian Jordan <ianjordan18@yahoo.com>
2020-02-25 20:12:35 -0500

    ui

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Books.css";
+import  {BookDisplay}  from "./BookDisplay";
 
-export default function Books() {
-    return (
-        <div className="Books">
-            <div className = "lander">
-                <h1>Books</h1>
-                <p>List of Books.</p>
+export default class Books extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id: 1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 2,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 3,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 4,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 5,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 6,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 7,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 8,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                }
+            ]
+        };
+    }
+
+
+    render () {
+        return (
+            <div className="Books">
+                <div className = "lander">
+                    <BookDisplay books = {this.state.books} />
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
